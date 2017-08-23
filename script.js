ac2a4979e33a5c96c8445590f8bba21895b3ed5f
AngeloSeolin <31291189+AngeloSeolin@users.noreply.github.com>
2017-08-23 17:43:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,34 +11,26 @@ function AlteraRele1(){
     var estado = document.getElementById("estado").innerHTML;
     if(estado === "1"){
         document.getElementById("botao").innerHTML="<a href='/Drive' class='botao'>Drive</a>";
-    } else {
-        document.getElementById("botao").innerHTML="<a href='/Drive' class='botao'>Drive</a>";
-    }
+    } 
 }
 
 function AlteraRele2(){
     var estado_2 = document.getElementById("estado_2").innerHTML;
     if(estado_2 === "1"){
         document.getElementById("botao_2").innerHTML="<a href='/Brake' class='botao_2'>Brake</a>";
-    } else {
-        document.getElementById("botao_2").innerHTML="<a href='/Brake' class='botao_2'>Brake</a>";
-    }
+    } 
 }
 
 function AlteraRele3(){
     var estado_3 = document.getElementById("estado_3").innerHTML;
     if(estado_3 === "1"){
         document.getElementById("botao_3").innerHTML="<a href='/Left' class='botao_3'>Left</a>";
-    } else {
-        document.getElementById("botao_3").innerHTML="<a href='/Left' class='botao_3'>Left</a>";
-    }
+    } 
 }
 
 function AlteraRele4(){
     var estado_4 = document.getElementById("estado_4").innerHTML;
     if(estado_4 === "1"){
-        document.getElementById("botao_4").innerHTML="<a href='/Right' class='botao_4'>Right</a>";
-    } else {
         document.getElementById("botao_4").innerHTML="<a href='/Right' class='botao_4'>Right</a>";
     }
 }
@@ -47,25 +39,19 @@ function AlteraRele5(){
     var estado_5 = document.getElementById("estado_5").innerHTML;
     if(estado_5 === "1"){
         document.getElementById("botao_5").innerHTML="<a href='/STOP' class='botao_5'>STOP</a>";
-    } else {
-        document.getElementById("botao_5").innerHTML="<a href='/STOP' class='botao_5'>STOP</a>";
-    }
+    } 
 }
 
 function AlteraRele6(){
     var estado_6 = document.getElementById("estado_6").innerHTML;
     if(estado_6 === "1"){
         document.getElementById("botao_6").innerHTML="<a href='/ON' class='botao_6'>Turn ON Engines</a>";
-    } else {
-        document.getElementById("botao_6").innerHTML="<a href='/ON' class='botao_6'>Turn ON Engines</a>";
-    }
+    } 
 }
 
 function AlteraRele7(){
     var estado_7 = document.getElementById("estado_7").innerHTML;
     if(estado_7 === "1"){
-        document.getElementById("botao_7").innerHTML="<a href='/OFF' class='botao_7'>Turn OFF Engines</a>";
-    } else {
         document.getElementById("botao_7").innerHTML="<a href='/OFF' class='botao_7'>Turn OFF Engines</a>";
     }
 }
